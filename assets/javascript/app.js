16f108ef083bf760a94e343fe2a79aa321775f94
sdreyes <sheshechan90@gmail.com>
2018-12-20 17:36:22 -0500

    Initialize Firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDm9Jku2wrP8xwQ8-Ys8cEKff9VWIXFXco",
+    authDomain: "trainscheduler-91a77.firebaseapp.com",
+    databaseURL: "https://trainscheduler-91a77.firebaseio.com",
+    projectId: "trainscheduler-91a77",
+    storageBucket: "trainscheduler-91a77.appspot.com",
+    messagingSenderId: "634435265553"
+  };
+  firebase.initializeApp(config);

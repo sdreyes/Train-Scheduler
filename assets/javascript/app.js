97032d311f61fa6fc3a553472ccd996210de6716
sdreyes <sheshechan90@gmail.com>
2018-12-22 08:32:38 -0500

    Add form validation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,55 @@
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyDm9Jku2wrP8xwQ8-Ys8cEKff9VWIXFXco",
     authDomain: "trainscheduler-91a77.firebaseapp.com",
     databaseURL: "https://trainscheduler-91a77.firebaseio.com",
     projectId: "trainscheduler-91a77",
     storageBucket: "trainscheduler-91a77.appspot.com",
     messagingSenderId: "634435265553"
-  };
-  firebase.initializeApp(config);
+};
+
+firebase.initializeApp(config);
+
 var database = firebase.database();
 var timeFormat = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
 
 $("#add-train-button").on("click", function(event) {
     event.preventDefault();
+
+    // $("#train-name").required = true;
+    // $("#train-destination").required = true;
+    // $("#train-time").required = true;
+    // $("#train-frequency").required = true;
+
+    $("#train-name").attr("required", true);
+
     trainName = $("#train-name").val().trim();
     trainDestination = $("#train-destination").val().trim();
     trainTime = $("#train-time").val().trim();
     trainFrequency = $("#train-frequency").val().trim();
+
     
 
-    if (!timeFormat.test(trainTime)) {
+    // Perform form validation before accepting values
+    if ($("#train-name").val() === "") {
+        alert("Train name is required!");
+    }
+    else if ($("#train-destination").val() === "") {
+        alert("Destination is required!");
+    }
+    else if ($("#train-time").val() === "") {
+        alert("First train time is required!");
+    }
+    else if (!timeFormat.test(trainTime)) {
         alert("Please input the time in HH:MM format (military time)");
+    }
+    else if ($("#train-frequency").val() === "") {
+        alert("Frequency is required!");
     }
     else if (!typeof trainFrequency === "number") {
         alert("Please enter the minutes in a numerical value");
     }
+
     else {
         database.ref().push({
             trainName: trainName,
@@ -33,7 +58,6 @@ $("#add-train-button").on("click", function(event) {
             trainFrequency: trainFrequency
         });
 
-        console.log(trainName, trainDestination, trainTime, trainFrequency);
     };
     
 });
